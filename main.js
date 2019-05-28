fa50b1a8bf0cc1ecfca163127e8cfcd2c80cba80
root <s2chjast@stud.eah-jena.de>
2019-05-28 14:52:08 +0200

    update

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 var MongoClient = require('mongodb').MongoClient;
 
+/*Es gibt 3 Collection, um mit einer zu arbeiten ersetzten Sie den Wert im Config.Objekt (Zeile 14)
+Collections:
+IndexId
+IndexIdTemp
+IndexIdTempFrame
+*/
+
 config={
 	"host":"10.81.41.71",
-	"port":"9999",
-	"db":"bigChart",
-    "user":"user",
-    "pass":"passwort",
-    "collectionMitTempFrameIndex":"collectionMitTempFrameIndex",
-    "collectionMitTempIndex":"collectionMitTempIndex",
-    "collectionMitFrameIndex":"collectionMitFrameIndex",
-    "collectionOhneIndex":"collectionOhneIndex"
+	"port":"4444",
+    "db":"bigChart",
+    "collection":"IndexIdTemp"
 }
-
-var collection=config.collectionOhneIndex;
 
 MongoClient.connect("mongodb://"+config.host+":"+config.port, { useNewUrlParser: true}, function(err, db){  
     if(err){ console.log( err);}  
     else{
         var dbObject=db.db(config.db);
-        dbObject.collection(collection).find({frame:280}).sort({temp:-1}).toArray(function(err, res){
-            
-            console.log(res[0]);
-
-            dbObject.collection(collection).find({frame:280}).sort({temp:-1}).explain(function(err, data){
-                console.log(data.executionStats.executionTimeMillis);
-                db.close();
-            });
+        var start=new Date();
+        dbObject.collection(config.collection).find({}).sort({temp:-1}).limit(1).toArray(function(err, res){
+            console.log(res);
+            console.log("Dauer in ms: ");
+            console.log(new Date()-start);
+            db.close();
         });
     }
 }); 
