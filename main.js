bedd0e2c17264f39e2e3b096cee578a8de6f28db
root <s2chjast@stud.eah-jena.de>
2019-06-03 15:40:09 +0200

    update

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ IndexIdTempFrame
 */
 
 config={
-	"host":"10.81.41.71",
-	"port":"4444",
+	"host":"127.0.0.1",
+	"port":"27017",
     "db":"bigChart",
     "collection":"IndexId"
 }
@@ -20,9 +20,15 @@ MongoClient.connect("mongodb://"+config.host+":"+config.port, { useNewUrlParser:
         var dbObject=db.db(config.db);
         var start=new Date();
         dbObject.collection(config.collection).find({}).sort({temp:-1}).limit(1).toArray(function(err, res){
-            console.log("Collection: "+config.collection+" "+res);
-            console.log("Dauer in ms: ");
-            console.log(new Date()-start);
+            if(err)
+            {
+                console.log(err);
+            }
+            
+            var dauer=new Date()-start;
+            console.log("Collection: "+config.collection);
+            console.log(res);
+            console.log("Dauer in ms: "+dauer);
             db.close();
         });
     }
